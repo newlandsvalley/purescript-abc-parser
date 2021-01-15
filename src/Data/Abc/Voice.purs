740d2eecade0dddc8e22edf5c24028867a7e39aa
John Watson <john.watson@gmx.co.uk>
2021-01-15 16:21:01 +0000

    Run Voice module in the Identity Monad

--- src/Data/Abc/Voice.purs
+++ src/Data/Abc/Voice.purs
@@ -24,16 +24,22 @@
 -- |
 -- | Of course, there is nothing in the spec which prevents degenerate cases where 
 -- | both forms are present.  In this case, we'll assume inline headers take precedence
+-- |
+-- | The strategy is to fold over the tune structure in the State monad.  State will be 
+-- | changed each time we come across a free-standing Voice header in the tune body.
+-- | The current voice is this unless over-ridden by an inline voice.  
+-- | The fold builds up a Map of Voices to partitioned ABC tunes
 module Data.Abc.Voice (partitionTuneBody) where
 
-import Prelude (class Eq, class Ord, ($), (<<<), join, map, not)
+import Prelude (class Eq, class Ord, ($), (<<<), join, map, not, pure)
 import Data.Abc (Bar, BodyPart(..), Header(..), Music(..), TuneBody)
 import Data.Abc.Metadata (isEmptyStave)
 import Data.List (List, head, singleton, snoc)
 import Data.Maybe (Maybe(..))
 import Data.Map (Map, empty, lookup, insert, toUnfoldable)
 import Data.Tuple (Tuple(..))
-import Data.Foldable (foldl)
+import Data.Foldable (foldM)
+import Data.Identity (Identity(..))
 
 data VoiceLabel = 
     VoiceLabel String
@@ -44,29 +50,54 @@ derive instance ordVoiceLabel :: Ord VoiceLabel
 
 type VoiceMap = Map VoiceLabel TuneBody
 
+type VoiceM = Identity
+
 partitionTuneBody :: TuneBody -> Array TuneBody
-partitionTuneBody b =
-  let
-    foldf :: VoiceMap -> BodyPart -> VoiceMap
-    foldf vmap bp =
+partitionTuneBody b =    
+  let 
+    voiceMap = runVoiceM (voiceFold b) 
+  in 
+    map (\(Tuple k v) -> v) $ toUnfoldable voiceMap
+
+runVoiceM :: forall a. VoiceM a -> a
+runVoiceM (Identity a) = a
+
+voiceFold :: TuneBody -> VoiceM VoiceMap
+voiceFold b = 
+  let  
+    foldf :: VoiceMap -> BodyPart -> VoiceM VoiceMap
+    foldf vmap bp = do
       case bp of
         BodyInfo header -> 
           case header of 
             Voice voiceDescription ->
-               addAtLabel (VoiceLabel voiceDescription.id) bp vmap
+              pure $ addAtLabel (VoiceLabel voiceDescription.id) bp vmap
             _ -> 
-               addToAll bp vmap
-        Score bars ->
+              pure $ addToAll bp vmap
+        Score bars -> do
           if (not $ isEmptyStave bars) then
-            let 
-              inlineLabel = scoreLabel bars
-            in
-              addAtLabel inlineLabel bp vmap
+            pure $ addAtLabel (scoreLabel bars) bp vmap
           else
-            vmap
-    voiceMap = foldl foldf (empty :: VoiceMap) b
+            pure vmap
   in
-    map (\(Tuple k v) -> v) $ toUnfoldable voiceMap
+    foldM foldf (empty :: VoiceMap) b
+
+{-}
+voiceFoldf :: VoiceMap -> BodyPart -> VoiceM VoiceMap
+voiceFoldf vmap bp = do
+  case bp of
+    BodyInfo header -> 
+      case header of 
+        Voice voiceDescription ->
+          pure $ addAtLabel (VoiceLabel voiceDescription.id) bp vmap
+        _ -> 
+          pure $ addToAll bp vmap
+    Score bars -> do
+      if (not $ isEmptyStave bars) then
+        pure $ addAtLabel (scoreLabel bars) bp vmap
+      else
+        pure vmap
+-}
 
 -- append a body part at the specified map label
 addAtLabel :: VoiceLabel -> BodyPart -> VoiceMap -> VoiceMap
